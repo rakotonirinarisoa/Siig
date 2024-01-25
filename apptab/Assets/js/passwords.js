c685f8fcee29dc5a61bcef4e791f23d76dcd865f
El-Cid-Campeador <mandaniainaandriamanantena@gmail.com>
2024-01-25 09:33:47 +0300

    25012024

--- apptab/Assets/js/passwords.js
+++ apptab/Assets/js/passwords.js
@@ -42,7 +42,7 @@ $('#get-user-password-btn').on('click', () => {
                 p.text('Identifiants incorrects!');
             } else {
                 p.css({ 'color': 'black' });
-                p.text(`${res.data.login}: ${res.data.password}`);
+                p.html(`Le mot de passe de <b>${res.data.login}</b> est <u>${res.data.password}</u>`);
 
                 closeButton.text('Fermer');
             }
