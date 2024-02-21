1962c8c032bafc45f0d56c2e120c9a25a80ec702
Rinah6 <fenosoaraharinosy@gmail.com>
2024-02-21 08:13:15 +0300

    RFR

--- apptab/Assets/js/TraitmandatLIAS.js
+++ apptab/Assets/js/TraitmandatLIAS.js
@@ -44,10 +44,17 @@ function modalLIAS(id) {
                 contentpaie = ``;
                 if (ListResult.REF != "" || ListResult.OBJ != "" || ListResult.TITUL != "") {
                     contentpaie += `<tr class="select-text">
-                                    <td style="font-weight: bold; text-align:center"><a href="${ListResult.REF}" target="_blank">${ListResult.REF}</a></td>
-                                    <td style="font-weight: bold; text-align:center"><a href="${ListResult.OBJ}" target="_blank">${ListResult.OBJ}</a></td>
-                                    <td style="font-weight: bold; text-align:center"><a href="${ListResult.TITUL}" target="_blank">${ListResult.TITUL}</a></td>
-                                </tr>`;
+                                        <td style="font-weight: bold; text-align:center">DEF</td>
+                                        <td style="font-weight: bold; text-align:center"><a href="${ListResult.REF}" target="_blank">${ListResult.REF}</a></td>
+                                    </tr>
+                                    <tr class="select-text">
+                                        <td style="font-weight: bold; text-align:center">TEF</td>
+                                        <td style="font-weight: bold; text-align:center"><a href="${ListResult.OBJ}" target="_blank">${ListResult.OBJ}</a></td>
+                                    </tr>
+                                    <tr class="select-text">
+                                        <td style="font-weight: bold; text-align:center">BE</td>
+                                        <td style="font-weight: bold; text-align:center"><a href="${ListResult.TITUL}" target="_blank">${ListResult.TITUL}</a></td>
+                                    </tr>`;
                 }
                 
                 $('.DOCMODAL').empty();
